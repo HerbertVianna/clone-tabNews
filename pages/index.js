2d4f6ab8d6635b0ddac539ac8387f347f6ac4a91
Herbert Vianna <herbertvianna@gmail.com>
2025-10-27 22:27:22 -0300

    Modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Com esse curso aqui eu vou virar um mago do front!</h1>
+    return <h1>Com esse curso aqui eu vou virar um mago do front!🧙🏼‍♂️</h1>
 }
 
 export default Home;
